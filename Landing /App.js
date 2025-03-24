f6a0de6a07963eab50cadb216ad2f645add6aa17
konso <ethioitclub@gmail.com>
2025-03-24 09:40:25 +0300

    ss

---
+++ Landing /App.js
@@ -0,0 +1,17 @@
+// client/src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Landing Page</h1>
+        <p>This is a simple landing page built with Node.js and React.</p>
+        <button>Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
